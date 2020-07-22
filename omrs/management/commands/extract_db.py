3fe94e915bd4f5b408f2beb145d2f8df86f20771
Jonathan Payne <paynejd@gmail.com>
2020-07-22 10:07:21 -0400

    Update extract_db.py

--- omrs/management/commands/extract_db.py
+++ omrs/management/commands/extract_db.py
@@ -6,17 +6,52 @@ Commands to create JSON for importing OpenMRS v1.11 concept dictionary into OCL.
 
     CREATE DATABASE ciel_20200706 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
-2. Update settings.py with database settings
+2. Update settings.py with your database settings, including the database (eg ciel_20200706),
+   database drivers, username, host, port, and password. name You will see a DATABASES constant
+   that looks something like this:
 
-3. Check sources in specified OCL environment:
+    DATABASES = {
+        'default': {
+            'NAME': 'ciel_20200706',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'HOST': 'localhost',
+            'PORT': '3307',
+            'PASSWORD': 'my-password',
+        }
+    }
 
-    python manage.py extract_db --check_sources --env=demo --token=<my-token-here>
+3. Verify that all of the OpenMRS dictionary's external mapping sources are defined in this code
+   repository's SOURCE_DIRECTORY (see /omrs/management/commands/__init__.py) and exist in specified
+   OCL environment:
 
-4a. Check sources and output as OCL-formatted bulk import JSON:
+        python manage.py extract_db --check_sources --env=demo --token=<my-token-here>
 
-    python manage.py extract_db --check_sources --env=demo --token=<my-token-here> --org_id=MyOrg --source_id=MySource -v0 --concepts --mappings --format=bulk > my_ocl_bulk_import_file.json
+   The SOURCE_DIRECTORY simply maps an OpenMRS external map source ID ('omrs_id') to a specific source
+   in OCL. In OCL, a source is identified by an 'ocl_id' (eg ICD-10-WHO), an 'owner_id' (eg 'WHO') and
+   an 'owner_type' (eg 'org' or 'user'). For example:
 
-5. Alternatively, create "old-style" OCL import scripts (separate for concept and mappings)
+        {'omrs_id':'ICD-10-WHO', 'owner_type':'org', 'owner_id':'WHO', 'ocl_id':'ICD-10-WHO'}
+
+   The SOURCE_DIRECTORY is unique to each OpenMRS dictionary, however in practice they are often
+   similar across OpenMRS dictionaries because many of them share CIEL as a common ancestor.
+   If a source is missing in the SOURCE_DIRECTORY, simply update or add a source mapping to reflect
+   the missing omrs_id.
+
+   If a source is missing in OCL, you may either add that source directly or contact an OCL
+   administrator.
+
+4. Update the MySql database model (/omrs/models.py) to reflect the version of the OpenMRS
+   dictionary you are importing. For example, OpenMRS v1.11 introduces a 'precise' field for
+   numeric fields, whereas older versions of OpenMRS do not. We recommend simply commenting
+   out fields that are not applicable to your OpenMRS version.
+
+5. Output as OCL-formatted bulk import JSON:
+
+    python manage.py extract_db --org_id=MyOrg --source_id=MySource -v0 --concepts --mappings
+        --format=bulk > my_ocl_bulk_import_file.json
+
+6. Alternatively, create "old-style" OCL import scripts (separate for concept and mappings)
     designed to be run directly on OCL server:
 
     python manage.py extract_db --org_id=MyOrg --source_id=MySource -v0 --concepts > concepts.json
