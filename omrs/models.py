bd0e2465d22e6b6c58cfd8444d25da643afcea98
Burke Mamlin <burke@openmrs.org>
2021-08-09 13:59:18 -0400

    Use allow_decimal instead of precise for numerics

--- omrs/models.py
+++ omrs/models.py
@@ -221,8 +221,8 @@ class ConceptNumeric(models.Model):
     low_normal = models.FloatField(blank=True, null=True)
     units = models.CharField(max_length=50, blank=True)
     # ConceptNumeric.precise was renamed to allow_decimal in Platform 2.2 (TRUNK-5333)
-    # precise = models.IntegerField()
     allow_decimal = models.IntegerField()
+    #allow_decimal = models.IntegerField(db_column='precise')
     display_precision = models.IntegerField()
 
     class Meta:
